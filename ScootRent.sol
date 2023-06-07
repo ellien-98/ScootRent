b3405e11267b82a018667bb03203802397e6f28d
Eleftheria Ntoulia <81044891+ellien-98@users.noreply.github.com>
2023-06-07 19:36:13 +0300

    Update ScootRent.sol

--- ScootRent.sol
+++ ScootRent.sol
@@ -111,12 +111,4 @@ contract ScootRent{
 
 
 
-/*
-Renter --> Rider
-checkOut --> takeScoot
-checkIn --> leaveScoot
-getTotalDuration--> calcTotalDuration
-renterTimespan --> calcTimespan
-balanceOf --> getBalance
-balanceOfRider --> balanceOfRenter
-*/
+
